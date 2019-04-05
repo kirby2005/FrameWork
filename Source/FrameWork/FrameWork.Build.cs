bc988fc736d98f7549cd2c18f552371b49687735
yuan-PC\yuan <a@a.a>
2019-04-05 22:13:07 +0800

    Lua integration: UE-3 集成到Framework项目

--- Source/FrameWork/FrameWork.Build.cs
+++ Source/FrameWork/FrameWork.Build.cs
@@ -1,10 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+using System;
+using System.IO;
 using UnrealBuildTool;
 
 public class FrameWork : ModuleRules
 {
-	public FrameWork(ReadOnlyTargetRules Target) : base(Target)
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/")); }
+    }
+
+    public FrameWork(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
@@ -12,12 +18,37 @@ public class FrameWork : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        LoadLua(Target); // This functions loads Lua
+    }
+
+    private bool LoadLua(ReadOnlyTargetRules TargetRules)
+    {
+        bool isLibSupported = false;
+
+        // Check if we are on Windows
+        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        {
+            isLibSupported = true;
+
+            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86"; // This string is either "x64" or "x86" so we can append it on the lib filename
+            string BuildTypeString = (Target.Configuration == UnrealTargetConfiguration.Debug) ? "Debug" : "Release";
+            string LibrariesPath = Path.Combine(ThirdPartyPath, "LuaIntegration", "libraries", PlatformString, BuildTypeString);
+
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "Lua5.1.5.lib"));
+
+            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "LuaIntegration", "includes"));
+        }
+
+        Definitions.Add(string.Format("WITH_LUA_BINDING={0}", isLibSupported ? 1 : 0));
+
+        return isLibSupported;
+    }
 }
